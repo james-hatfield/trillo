3f044fc3a055676da8111c255a646c4731b03520
James Hatfield <jhatfield209@gmail.com>
2020-08-14 09:37:28 -0700

    added custom SVGs

---
+++ src/components/SVG/BookmarkSVG.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const BookmarkSVG = () => (
+  <svg
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
+  >
+    <title>bookmark</title>
+    <path d="M6 0v32l10-10 10 10v-32z"></path>
+  </svg>
+);
+export default BookmarkSVG;
